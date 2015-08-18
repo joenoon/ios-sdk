fee724928a0717a6cea269d6062b498c39e0c38a
Dal Rupnik <legoless@gmail.com>
2015-08-18 09:31:21 +0200

    Created the project

---
+++ YesGraphSDK/YesGraphSDK/YesGraphSDK.h
@@ -0,0 +1,19 @@
+//
+//  YesGraphSDK.h
+//  YesGraphSDK
+//
+//  Created by Dal Rupnik on 18/08/15.
+//  Copyright (c) 2015 YesGraph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YesGraphSDK.
+FOUNDATION_EXPORT double YesGraphSDKVersionNumber;
+
+//! Project version string for YesGraphSDK.
+FOUNDATION_EXPORT const unsigned char YesGraphSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YesGraphSDK/PublicHeader.h>
+
+
