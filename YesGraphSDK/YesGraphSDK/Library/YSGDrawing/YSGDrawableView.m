fca757e4fe3ef0e06fecca45b5d6d99a8706a945
Dal Rupnik <legoless@gmail.com>
2015-09-07 15:38:18 +0200

    Added drawing code

---
+++ YesGraphSDK/YesGraphSDK/Library/YSGDrawing/YSGDrawableView.m
@@ -0,0 +1,21 @@
+//
+//  YSGDrawableView.m
+//  YesGraphSDK
+//
+//  Created by Dal Rupnik on 07/09/15.
+//  Copyright © 2015 YesGraph. All rights reserved.
+//
+
+#import "YSGDrawableView.h"
+
+@implementation YSGDrawableView
+
+- (void)drawRect:(CGRect)rect
+{
+    if (self.drawingBlock)
+    {
+        self.drawingBlock(rect, nil);
+    }
+}
+
+@end
---
+++ YesGraphSDK/YesGraphSDK/Library/YSGDrawing/YSGDrawing.h
@@ -0,0 +1,10 @@
+//
+//  YSGDrawing.h
+//  YesGraphSDK
+//
+//  Created by Dal Rupnik on 07/09/15.
+//  Copyright © 2015 YesGraph. All rights reserved.
+//
+
+#import "YSGDrawingConstants.h"
+#import "YSGDrawableView.h"
