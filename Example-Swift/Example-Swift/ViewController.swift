5178ee02c74fc46674adfafffcb0f6e9baabfaae
Dal Rupnik <legoless@gmail.com>
2015-09-08 14:50:28 +0200

    Added Swift Project

---
+++ Example-Swift/Example-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-Swift
+//
+//  Created by Dal Rupnik on 08/09/15.
+//  Copyright © 2015 Dal Rupnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
