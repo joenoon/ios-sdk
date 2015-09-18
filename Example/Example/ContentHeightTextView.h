163cd3c634ea248a6b20ad27b5fef25e5b084177
mgresak <miha.gresak@mobiliti.si>
2015-09-18 14:01:55 +0200

    Solved auto layout issues, textview height automatically adjusts to text, styled launch screens

---
+++ Example/Example/ContentHeightTextView.h
@@ -0,0 +1,20 @@
+//
+//  ContentHeightTextView.h
+//  Example
+//
+//  Created by Miha Gresak on 18/09/15.
+//  Copyright © 2015 YesGraph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentHeightTextView : UITextView
+
+@end
+
+
+@interface ContentHeightTextView ()
+
+@property (nonatomic, strong) NSLayoutConstraint *heightConstraint;
+
+@end
---
+++ Example/Example/ContentHeightTextView.m
@@ -0,0 +1,29 @@
+//
+//  ContentHeightTextView.m
+//  Example
+//
+//  Created by Miha Gresak on 18/09/15.
+//  Copyright © 2015 YesGraph. All rights reserved.
+//
+
+#import "ContentHeightTextView.h"
+
+@implementation ContentHeightTextView
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGSize size = [self sizeThatFits:CGSizeMake(self.bounds.size.width, FLT_MAX)];
+    
+    if (!self.heightConstraint) {
+        self.heightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0f constant:size.height];
+        [self addConstraint:self.heightConstraint];
+    }
+    
+    self.heightConstraint.constant = size.height;
+    
+    [super layoutSubviews];
+}
+
+@end
