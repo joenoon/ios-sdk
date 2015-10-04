e3c1f5ca531a709be9797f6aa7b182ca8963f362
Nejc Vivod <nejc.vivod@mobiliti.si>
2015-10-04 23:03:35 +0200

    Forgot to remove on const qualifier

--- YesGraphSDK/YesGraphSDK/Theme/YSGThemeConstants.m
+++ YesGraphSDK/YesGraphSDK/Theme/YSGThemeConstants.m
@@ -35,7 +35,7 @@
     return [UIColor colorWithRed:0.33 green:0.33 blue:0.33 alpha:1];
 }
 
-+ (nonnull const NSString *)defaultFontFamily
++ (nonnull NSString *)defaultFontFamily
 {
     return [UIFont systemFontOfSize:12].familyName;
 }
