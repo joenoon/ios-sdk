23ea9ee9e27829c78941a170b164a63b4f0dafdc
Dal Rupnik <legoless@gmail.com>
2015-10-09 14:49:27 +0200

    Inclusion fixes

--- YesGraph/YesGraphSDK/YesGraphSDK/YesGraphSDK.h
+++ YesGraph/YesGraphSDK/YesGraphSDK/YesGraphSDK.h
@@ -14,17 +14,17 @@ FOUNDATION_EXPORT double YesGraphSDKVersionNumber;
 //! Project version string for YesGraphSDK.
 FOUNDATION_EXPORT const unsigned char YesGraphSDKVersionString[];
 
-#import <YSGConstants.h>
-#import <YSGUtility.h>
-#import <YesGraph.h>
-#import <YSGClient.h>
-#import <YSGShareService.h>
-#import <YSGShareSheetController.h>
-#import <YSGAddressBook.h>
-#import <YSGTheme.h>
+#import "YSGConstants.h"
+#import "YSGUtility.h"
+#import "YesGraph.h"
+#import "YSGClient.h"
+#import "YSGShareService.h"
+#import "YSGShareSheetController.h"
+#import "YSGAddressBook.h"
+#import "YSGTheme.h"
 
-#import <YSGSources.h>
-#import <YSGServices.h>
-#import <YSGModel.h>
-#import <YSGParsing.h>
-#import <YSGDrawing.h>
+#import "YSGSources.h"
+#import "YSGServices.h"
+#import "YSGModel.h"
+#import "YSGParsing.h"
+#import "YSGDrawing.h"
