424362ba5ca918184a563d822d1acd98b4b574c0
Nejc Vivod <nejc.vivod@mobiliti.si>
2015-10-15 00:53:27 +0200

    Added test for grouped suggestions

--- YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKModelYSGContactListTests.m
+++ YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKModelYSGContactListTests.m
@@ -25,6 +25,24 @@
     [super tearDown];
 }
 
+- (void)testContactListOperationsSorted
+{
+    YSGContactList *mockedContacts = [YSGTestMockData mockContactList];
+    NSDictionary <NSString *, NSArray <YSGContact *> *> *noSuggestions = [mockedContacts sortedEntriesWithNumberOfSuggestions:0];
+    XCTAssertNotNil(noSuggestions, @"Sorted contacts shouldn't be nil");
+    NSArray *expectedSections = @[ @{ @"M": @1 }, @{ @"R": @1 }, @{ @"P": @1 }, @{ @"S": @2 }, @{ @"T": @1 }, @{ @"H": @1 }, @{ @"C": @1 } ];
+    XCTAssertEqual(noSuggestions.allKeys.count, expectedSections.count, @"There should be '%lu' groups in contacts, but found '%lu'", expectedSections.count, noSuggestions.allKeys.count);
+    for (NSUInteger index = 0; index < expectedSections.count; ++index)
+    {
+        NSDictionary *expectation = expectedSections[index];
+        NSString *key = expectation.allKeys.firstObject;
+        NSArray *contacts = [noSuggestions objectForKey:key];
+        XCTAssertNotNil(contacts, @"Contacts shouldn't be nil for key '%@'", key);
+        NSNumber *val = [expectation objectForKey:key];
+        XCTAssertEqual(contacts.count, [val integerValue], @"Number of contacts in section '%@' should be '%@', but it was '%lu'", key, val, contacts.count);
+    }
+}
+
 - (void)testContactListOperationsSuggestions
 {
     YSGContactList *mockedContacts = [YSGTestMockData mockContactList];
