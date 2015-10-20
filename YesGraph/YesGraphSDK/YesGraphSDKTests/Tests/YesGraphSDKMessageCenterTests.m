158adfc161f9ee46546099a631f103e79a2d56f9
Nejc Vivod <nejc.vivod@mobiliti.si>
2015-10-20 23:46:38 +0200

    Added error handler test as well

--- YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKMessageCenterTests.m
+++ YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKMessageCenterTests.m
@@ -35,7 +35,7 @@
     __weak YSGMessageCenter *preventRetainCycleInstance = self.center;
     preventRetainCycleInstance.messageHandler = ^(NSString *message, NSDictionary * _Nullable userInfo)
      {
-         // XCTAssert([message isEqualToString:sentMessage], @"The sent message '%@' and received message '%@' are not the same", sentMessage, message);
+         XCTAssert([message isEqualToString:sentMessage], @"The sent message '%@' and received message '%@' are not the same", sentMessage, message);
          XCTAssertNil(userInfo, @"User info should be nil, not '%@'", userInfo);
          [expectation fulfill];
      };
@@ -44,6 +44,28 @@
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * _Nullable error)
      {
          XCTAssertNil(error, @"Error should be nil not '%@', otherwise the message handler was never invoked", error);
+     }];
+}
+
+- (void)testSendErrorWithHandlerBlock
+{
+    NSError *sentError = [NSError errorWithDomain:@"YesGraphSDKDomain" code:22 userInfo:nil];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Expect Error Handler To Be Invoked"];
+
+    __weak YSGMessageCenter *preventRetainCycleInstance = self.center;
+    preventRetainCycleInstance.errorHandler = ^(NSError *error)
+     {
+         XCTAssertNotNil(error, @"Sent error shouldn't be nil");
+         XCTAssert([sentError.domain isEqualToString:error.domain], @"Error domain '%@' is not the same as sent domain '%@'", error.domain, sentError.domain);
+         XCTAssertEqual(sentError.code, error.code, @"Error code '%ld' is not the same as sent code '%lu'", (long)error.code,  (long)sentError.code);
+         XCTAssert([sentError.userInfo isEqualToDictionary:error.userInfo], @"User info '%@' is not the same as sent '%@'", error.userInfo, sentError.userInfo);
+         [expectation fulfill];
+     };
+
+    [self.center sendError:sentError];
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * _Nullable error)
+     {
+         XCTAssertNil(error, @"Error should be nil not '%@', otherwise the error handler was never invoked", error);
      }];
 }
 
