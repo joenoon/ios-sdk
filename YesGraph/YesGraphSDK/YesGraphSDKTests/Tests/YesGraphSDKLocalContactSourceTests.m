41a4a74427b8c62580c85b461db862278e38929c
Nejc Vivod <nejc.vivod@mobiliti.si>
2015-10-26 22:33:12 +0100

    Added tests for local contact source

--- YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKLocalContactSourceTests.m
+++ YesGraph/YesGraphSDK/YesGraphSDKTests/Tests/YesGraphSDKLocalContactSourceTests.m
@@ -7,11 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "YSGLocalContactSource.h"
+#import "YSGLocalContactSource+ExposePrivateMethods.h"
 #import "YSGContactList.h"
 #import "YSGTestMockData.h"
 
 @interface YesGraphSDKLocalContactSourceTests : XCTestCase
+@property (strong, nonatomic) YSGLocalContactSource *localSource;
 @end
 
 @implementation YesGraphSDKLocalContactSourceTests
@@ -19,17 +20,18 @@
 - (void)setUp
 {
     [super setUp];
+    self.localSource = [YSGLocalContactSource new];
 }
 
 - (void)tearDown
 {   
     [super tearDown];
+    self.localSource = nil;
 }
 
 - (void)testLocalContactStoreFetchNil
 {
-    YSGLocalContactSource *localSource = [YSGLocalContactSource new];
-    [localSource fetchContactListWithCompletion:^(YSGContactList * _Nullable contactList, NSError * _Nullable error)
+    [self.localSource fetchContactListWithCompletion:^(YSGContactList * _Nullable contactList, NSError * _Nullable error)
     {
         XCTAssertNil(error, @"Error should be nil, not '%@'", error);
         XCTAssertEqual(contactList.entries.count, 0, @"There shouldn't be any entries in the contacts list");
@@ -39,6 +41,16 @@
 - (void)testHasPermission
 {
     XCTAssertTrue([YSGLocalContactSource hasPermission], @"We shouldn have contact permissions");
+}
+
+- (void)testUserDefaults
+{
+    XCTAssertEqual([NSUserDefaults standardUserDefaults], [YSGLocalContactSource userDefaults], @"User defaults from local source should be the same as standard user defaults");
+}
+
+- (void)testPromptTitle
+{
+    XCTAssert([self.localSource.contactAccessPromptTitle isEqualToString:@"Invite friends"], @"Access prompt title shouldn't be '%@'", self.localSource.contactAccessPromptTitle);
 }
 
 @end
