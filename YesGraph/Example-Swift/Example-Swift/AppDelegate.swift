ac372569fe8049b35d9c23c297bb12a8a7c478de
Dal Rupnik <legoless@gmail.com>
2015-12-17 12:38:22 +0100

    Updated Swift example app

--- YesGraph/Example-Swift/Example-Swift/AppDelegate.swift
+++ YesGraph/Example-Swift/Example-Swift/AppDelegate.swift
@@ -7,10 +7,38 @@
 //
 
 import UIKit
+import YesGraphSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
+        //
+        // Configures YesGraph with a specific User ID and Source, so we know where are contacts from.
+        //
+        if YesGraph.shared().userId == nil {
+            YesGraph.shared().configureWithUserId(YSGUtility.randomUserId())
+            
+            //
+            // Configuring the source of contacts really helps us working with contacts.
+            //
+            /*let source = YSGSource()
+            source.name = "Name"
+            source.email = "Email"
+            source.phone = "+1 123 123 123"
+            
+            YesGraph.shared().contactOwnerMetadata = source*/
+        }
+        
+        //
+        // Client key should be retrieved from your trusted backend and is cached in the YesGraph SDK.
+        // If user logins with another user ID, new client key must be configured.
+        //
+        YesGraph.shared().configureWithClientKey("")
+        
+        return true
+    }
 }
